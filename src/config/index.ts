0b0af7895eb61ed5b3147746b2b2b52df5688034
berges99 <daviid.bergees@gmail.com>
2026-02-09 16:00:13 +0100

    Removed BASE_URL env var.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,6 @@
 export const config = {
   port: Number(process.env.PORT) || 3000,
   env: process.env.NODE_ENV || "development",
-  baseUrl: process.env.BASE_URL || "http://localhost:3000",
   api: {
     title: "Blueprint API",
     version: "1.0.0",
